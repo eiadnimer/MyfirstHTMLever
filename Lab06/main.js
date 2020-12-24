72d8eb7a79554d5db00f3389c342a83676110446
eiadnimer <eiadnimer_swaidat@yahoo.com>
2020-12-24 17:13:18 +0200

    adding js for the index file

--- Lab06/main.js
+++ Lab06/main.js
@@ -1,4 +1,16 @@
 var user = prompt("Enter you loyalty");
 if (user === "Madridista")
- { alert(" welcome to your home " + user)}
+{ alert(" welcome to your home " + user)}
 else{ alert(" GO OUT")}
+while (user !== 'Madridista'){
+user = prompt('You must be a Madridista to enter our page');
+}
+alert("You are in the right place dude")
+var fan = prompt(' how many goalkeeper we have on our squad ?');
+while (fan !== '3'){
+fan = prompt(' how many goalkeeper we have on our squad ?');
+}
+alert("hala 3me")
+for ( var i = 0; i<fan; i++){
+document.write('<img src="GK.jpg">')
+}

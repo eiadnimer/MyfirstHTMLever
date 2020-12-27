e2a4dc7d4b5b9083872eda5597debf78ca5cb6fc
eiadnimer <eiadnimer_swaidat@yahoo.com>
2020-12-27 15:47:21 +0200

    adding Lab5 & Lab7

--- Lab06/main.js
+++ Lab06/main.js
@@ -2,10 +2,13 @@ var user = prompt("Enter you loyalty");
 if (user === "Madridista")
 { alert(" welcome to your home " + user)}
 else{ alert(" GO OUT")}
-while (user !== 'Madridista'){
-user = prompt('You must be a Madridista to enter our page');
+Madridistaonly(user);
+function Madridistaonly (uName){
+    while (uName !== 'Madridista'){
+        uName = prompt('You must be a Madridista to enter our page');
+        }
+        alert("You are in the right place dude")
 }
-alert("You are in the right place dude")
 var fan = prompt(' how many goalkeeper we have on our squad ?');
 while (fan !== '3'){
 fan = prompt(' how many goalkeeper we have on our squad ?');
